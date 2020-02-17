8a24b8c2cc628d423224d9886000f687a644cad3
VolgaKurvar <kurvan1112@gmail.com>
2020-02-17 23:57:40 +0900

    不要な;を排除

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -14,7 +14,7 @@ use Cake\Event\Event;
  */
 class UsersController extends AppController
 {
-    public function initialize():
+    public function initialize()
     {
         parent::initialize();
         $this->loadComponent('RequestHandler');
